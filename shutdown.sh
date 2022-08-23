4d872925739443a0dad41f1b954625f234ecf567
darenweimer <darenweimer@gmail.com>
2022-08-23 15:09:21 -0700

    Added redmine to startup and shutdown commands

--- shutdown.sh
+++ shutdown.sh
@@ -11,3 +11,4 @@ docker compose -f nginx-proxy/docker-compose.yml down
 #-------------------------------------------------------------------------------
 
 docker compose -f jenkins/docker-compose.yml -f jenkins/docker-compose.override.yml down
+docker compose -f redmine/docker-compose.yml -f redmine/docker-compose.override.yml down
--- startup.sh
+++ startup.sh
@@ -11,3 +11,4 @@ docker compose -f nginx-proxy/docker-compose.yml up -d
 #-------------------------------------------------------------------------------
 
 docker compose -f jenkins/docker-compose.yml -f jenkins/docker-compose.override.yml up -d
+docker compose -f redmine/docker-compose.yml -f redmine/docker-compose.override.yml up -d
