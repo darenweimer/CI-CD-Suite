00941775467fc86f931aebd4d8b33e8362b693d0
darenweimer <darenweimer@gmail.com>
2022-09-15 09:49:47 -0700

    Changed order of applications in startup and shutdown scripts

--- startup.sh
+++ startup.sh
@@ -10,6 +10,6 @@ docker compose -f nginx-proxy/docker-compose.yml up -d
 # Startup additional software
 #-------------------------------------------------------------------------------
 
+docker compose -f vault/docker-compose.yml -f vault/docker-compose.override.yml up -d
 docker compose -f jenkins/docker-compose.yml -f jenkins/docker-compose.override.yml up -d
 docker compose -f redmine/docker-compose.yml -f redmine/docker-compose.override.yml up -d
-docker compose -f vault/docker-compose.yml -f vault/docker-compose.override.yml up -d
